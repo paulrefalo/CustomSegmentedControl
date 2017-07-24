d2002a59ffca27b2e889ed8647bc7e7ecd84d7c1
Paul ReFalo <paul.refalo@gmail.com>
2017-07-23 23:39:59 -0700

    docs:  Added designable file
    Added designable file, font, and organized groups

---
+++ CustomSegmentedControl/CustomSegmentedControl/UISegmentedControlX.swift
@@ -0,0 +1,110 @@
+//
+//  UISegmentedControlX.swift
+//  CustomSegmentedControl
+//
+//  Created by Paul ReFalo on 7/23/17.
+//  Copyright © 2017 QSS. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomSegmentedControl: UIControl {
+    var buttons = [UIButton]()
+    var selector: UIView!
+    var selectedSegmentIndex = 0
+    
+    @IBInspectable
+    var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor = UIColor.clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var commaSeparatedButtonTitles: String = ""
+    
+    @IBInspectable
+    var textColor: UIColor = .lightGray
+    
+    @IBInspectable
+    var selectorColor: UIColor = .darkGray
+    
+    @IBInspectable
+    var selectorTextColor: UIColor = .white
+    
+    func updateView() {
+        buttons.removeAll()
+        subviews.forEach { $0.removeFromSuperview() }
+        
+        let buttonTitles = commaSeparatedButtonTitles.components(separatedBy: ",")
+        
+        if buttonTitles[0] == "" { return }
+        
+        for buttonTitle in buttonTitles {
+            let button = UIButton(type: .system)
+            button.setTitle(buttonTitle, for: .normal)
+            // Allow longer titles to wrap if needed
+            button.titleLabel?.lineBreakMode = .byWordWrapping
+            button.titleLabel?.numberOfLines = 2
+            button.titleLabel?.textAlignment = .center
+            button.setTitleColor(textColor, for: .normal)
+            button.addTarget(self, action: #selector(buttonTapped(button:)), for: .touchUpInside)
+            buttons.append(button)
+        }
+        
+        buttons[0].setTitleColor(selectorTextColor, for: .normal)
+        
+        let selectorWidth = frame.width / CGFloat(buttonTitles.count)
+        selector = UIView(frame: CGRect(x: 0, y: 0, width: selectorWidth, height: frame.height))
+        selector.layer.cornerRadius = frame.height / 2
+        selector.backgroundColor = selectorColor
+        addSubview(selector)
+        
+        let sv = UIStackView(arrangedSubviews: buttons)
+        sv.axis = .horizontal
+        sv.alignment = .fill
+        sv.distribution = .fillEqually
+        addSubview(sv)
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        sv.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        sv.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        sv.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+    }
+    
+    override func draw(_ rect: CGRect) {
+        layer.cornerRadius = frame.height / 2
+        updateView()
+    }
+    
+    override func layoutSubviews() {
+        updateView()
+    }
+    
+    func buttonTapped(button: UIButton) {
+        for (buttonIndex, btn) in buttons.enumerated() {
+            btn.setTitleColor(textColor, for: .normal)
+            
+            if btn == button {
+                selectedSegmentIndex = buttonIndex
+                let selectorStartPosition = frame.width/CGFloat(buttons.count) * CGFloat(buttonIndex)
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.selector.frame.origin.x = selectorStartPosition
+                })
+                
+                btn.setTitleColor(selectorTextColor, for: .normal)
+            }
+        }
+        
+        sendActions(for: .valueChanged)
+    }
+    
+}
