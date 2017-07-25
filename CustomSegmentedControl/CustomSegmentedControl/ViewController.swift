f45cfed572692341233d87bae4412cacfc5909d8
Paul ReFalo <paul.refalo@gmail.com>
2017-07-24 21:13:16 -0700

    docs:  Clean up main View Controller
    Clean up main view controller and debug issues.  Still having trouble
    getting Designable to work.

--- CustomSegmentedControl/CustomSegmentedControl/ViewController.swift
+++ CustomSegmentedControl/CustomSegmentedControl/ViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+/*
     @IBOutlet weak var departingView: UIView!
     @IBOutlet weak var returningView: UIView!
     @IBOutlet weak var addCityButton: UIButtonX!
     @IBOutlet weak var cloudsImageView: UIImageView!
     @IBOutlet weak var searchButton: UIButtonX!
     @IBOutlet weak var agreeSwitch: UISwitch!
+ */
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        /*
         agreeSwitch.isOn = false
         searchButton.isEnabled = false
         
@@ -28,61 +30,72 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 55, delay: 0, options: [.autoreverse, .curveLinear], animations: {
             self.cloudsImageView.transform = CGAffineTransform(translationX: -(self.cloudsImageView.frame.width-self.view.frame.width), y: 0)
         })
+         */
     }
     
     @IBAction func SegmentChanged(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
         case 0:
             UIView.animate(withDuration: 0.3, animations: {
+                /*
                 self.departingView.transform = .identity
                 self.returningView.transform = .identity
                 self.addCityButton.alpha = 0
+                */
             })
             break
         case 1:
             UIView.animate(withDuration: 0.3, animations: {
+                /*
                 self.returningView.transform = CGAffineTransform(translationX: self.returningView.frame.width + 50, y: 0)
                 let x = self.view.center.x - self.departingView.center.x
                 self.departingView.transform = CGAffineTransform(translationX: x, y: 0)
                 self.addCityButton.alpha = 0
+                 */
             })
             break
         default:
             // 3rd Segment
             UIView.animate(withDuration: 0.3, animations: {
-                self.addCityButton.alpha = 1
+                // self.addCityButton.alpha = 1
             })
             break
         }
     }
     
-    
+    /*
     @IBAction func customSegmentValueChanged(_ sender: CustomSegmentedControl) {
         switch sender.selectedSegmentIndex {
         case 0:
             UIView.animate(withDuration: 0.3, animations: {
+                /*
                 self.departingView.transform = .identity
                 self.returningView.transform = .identity
                 self.addCityButton.alpha = 0
+                */
             })
             break
         case 1:
             UIView.animate(withDuration: 0.3, animations: {
+                /*
                 self.returningView.transform = CGAffineTransform(translationX: self.returningView.frame.width + 50, y: 0)
                 let x = self.view.center.x - self.departingView.center.x
                 self.departingView.transform = CGAffineTransform(translationX: x, y: 0)
                 self.addCityButton.alpha = 0
+                 */
             })
             break
         default:
             // 3rd Segment
             UIView.animate(withDuration: 0.3, animations: {
-                self.addCityButton.alpha = 1
+                // self.addCityButton.alpha = 1
             })
             break
         }
     }
+    */
     
+    /*
     @IBAction func flexibleDays(_ sender: UIButtonX) {
         if sender.backgroundColor == .clear {
             sender.backgroundColor = .lightGray
@@ -96,6 +109,7 @@ class ViewController: UIViewController {
     @IBAction func agreeSwitched(_ sender: UISwitch) {
         searchButton.isEnabled = sender.isOn
     }
+    */
 }
 
 
